6f975848515a1f2e4905431662b191944e63455e
zentralschweiz <zentralschweiz@gmx.ch>
2017-08-06 17:19:05 +0200

    geändert

--- JavaProjec/Hello.java
+++ JavaProjec/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
 	public static void main(String[] args){
+		String j = "";
 		for(int i=1; i<=10; i++){
-			System.out.println("Hello-"+i“—2“);
+			j = j + i;
+			System.out.println("Hello-" + i + "—" + j);
 		}
 	}
 }
